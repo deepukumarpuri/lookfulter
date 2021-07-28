dcd29cd25ec3b7abbe86e878513a2d9a0d655f38
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-07-28 16:23:52 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -6,7 +6,7 @@ class Translation(object):
     
     START_TEXT = """<b>Hey {} How are You!!</b>
 <i>🙋‍♂️ Hello
-I AM NETFLIX MOVIE/SERIAL UPLOADER BOT. YOU WANT  NETFLIX MOVIE/SERIAL GO MY GROUP :- @ONLYSERIES_76.SEND /help TO KNOW MORE.
+I AM ALL TYPES OF MOVIE/SERIAL UPLOADER BOT. YOU WANT  NETFLIX MOVIE/SERIAL GO MY GROUP :- @HINDIMOVIEWEBSERIESNETFLIX.SEND /help TO KNOW MORE.
 ALL TYPES OF MOVIE AVALIABLE MY CHANNEL:- @ALLSERIESMOVIESHD
 BHOJPURI CHANEL :- @HD_BHOJPURI_MOVIE
 </i>"""    
@@ -20,11 +20,11 @@ BOT SEND MOVIE/SERIAL
     
     ABOUT_TEXT = """<b>➥ My Name</b> : <code> DK MOVIE/SERIAL UPLOADER BOT</code>
     
-<b>➥ My Creator</b> : <b><i><a href="https://t.me/DKBOTZHELP">RAFEEQ</a></i></b>
+<b>➥ My Creator</b> : <b><i><a href="https://t.me/DKBOTZHELP">ANONYMOSE</a></i></b>
 
 <b>➥ My Language</b> : <code>Python3</code>
 
 <b>➥ My Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ MY GROUP </b> : <i><a href="https://t.me/onlyseries_76">Click Me</a></i>
+<b>➥ MY GROUP </b> : <i><a href="https://t.me/HINDIMOVIEWEBSERIESNETFLIX">Click Me</a></i>
 """
